257427bbaa32e6b1309d124a61d97cf45ebf12ff
Mathias Payer <mathias.payer@nebelwelt.net>
2021-01-04 18:49:50 +0100

    Deleted obsolete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget -c https://dblp.uni-trier.de/xml/dblp.dtd
-wget -c https://dblp.uni-trier.de/xml/dblp.xml.gz
-gunzip dblp.xml.gz
-wget -c https://raw.githubusercontent.com/emeryberger/CSrankings/gh-pages/dblp-aliases.csv
-mkdir -p www
